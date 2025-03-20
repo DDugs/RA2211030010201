3f626c99a2b787d476637f5304e5c91bc2775fd4
Dhruv Vishesh Gupta <120174191+DDugs@users.noreply.github.com>
2025-03-20 19:45:38 +0530

    changes

--- Average_Calculator_Microservice/fetch.py
+++ Average_Calculator_Microservice/fetch.py
@@ -2,18 +2,16 @@ import requests
 
 url = "http://20.244.56.144/test/register"
 data = {
-    "companyName": "SRM University",
-    "ownerName": "Dhruv Gupta",
-    "rollNo": "RA2211030010201",
-    "ownerEmail": "dg7950@srmist.edu.in",
-    "accessCode": "SUfGJv"
+    "companyName": "company",
+    "ownerName": "name",
+    "rollNo": "roll",
+    "ownerEmail": "email",
+    "accessCode": "token"
 }
 
 response = requests.post(url, json=data)
 
 print("Response Status Code:", response.status_code)
-print("Response Body:", response.json())
-
-
+print("Response Body:", response.json()) 
 
 
